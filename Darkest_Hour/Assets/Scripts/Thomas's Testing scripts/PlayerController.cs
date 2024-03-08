285a03ee5ae980f60d81c0b36e5159cda6288b2e
Dario Lopez <DSLopez1@student.fullsail.edu>
2024-03-07 19:34:40 -0800

    Updated player Script

--- Darkest_Hour/Assets/Scripts/Thomas's Testing scripts/PlayerController.cs
+++ Darkest_Hour/Assets/Scripts/Thomas's Testing scripts/PlayerController.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IPhysics
 {
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] public float playerSpeed = 5f;
     [SerializeField] float rotationSpeed = 500f;
+    [SerializeField] public Transform firePos;
+    [SerializeField] private float physResolve;
 
     Quaternion targetRotation;
 
     CameraController cameraController;
     Animator animator;
     CharacterController characterController;
+
+    private Vector3 _moveDir;
+    private Vector3 _pushBack;
 
     private void Awake()
     {
@@ -22,6 +31,9 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+
+        _pushBack = Vector3.Lerp(_pushBack, Vector3.zero, physResolve * Time.deltaTime);
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
@@ -29,16 +41,27 @@ public class PlayerController : MonoBehaviour
 
         var moveInput = (new Vector3(h, 0, v)).normalized;
 
-        var moveDir = cameraController.PlanarRotation * moveInput;
+        _moveDir = (cameraController.PlanarRotation * moveInput) * playerSpeed;
 
         if (moveAmount > 0)
         {
-            characterController.Move(moveDir * moveSpeed * Time.deltaTime);
-            targetRotation = Quaternion.LookRotation(moveDir);
+            characterController.Move((_moveDir + _pushBack) * Time.deltaTime);
+
+            targetRotation = Quaternion.LookRotation(_moveDir);
         }
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
         animator.SetFloat("moveAmount", moveAmount, 0.2f, Time.deltaTime);
+    }
+
+    public Vector3 getMoveVec()
+    {
+        return _moveDir;
+    }
+
+    public void PhysicsDir(Vector3 dir)
+    {
+        _pushBack += dir;
     }
 }
