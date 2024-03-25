98f25eb647654ff36233a068a05fe551580b2c7a
Corey Bell Wright <CJBellWright@student.fullsail.edu>
2024-03-24 21:36:22 -0400

    Bugs

--- Darkest_Hour/Assets/Scripts/Thomas's Testing scripts/PlayerTele.cs
+++ Darkest_Hour/Assets/Scripts/Thomas's Testing scripts/PlayerTele.cs
@@ -12,17 +12,17 @@ public class PlayerTeleportPortal : MonoBehaviour
             GameObject player = other.gameObject;
             DontDestroyOnLoad(player);
 
-            GameObject[] healthBars = GameObject.FindGameObjectsWithTag("HealthBar");
-            foreach (GameObject healthBar in healthBars)
-            {
-                DontDestroyOnLoad(healthBar);
-            }
+            //GameObject[] healthBars = GameObject.FindGameObjectsWithTag("HealthBar");
+            //foreach (GameObject healthBar in healthBars)
+            //{
+            //    DontDestroyOnLoad(healthBar);
+            //}
 
-            GameObject livesCounter = GameObject.FindGameObjectWithTag("LivesCounter");
-            if (livesCounter != null)
-            {
-                DontDestroyOnLoad(livesCounter);
-            }
+            //GameObject livesCounter = GameObject.FindGameObjectWithTag("LivesCounter");
+            //if (livesCounter != null)
+            //{
+            //    DontDestroyOnLoad(livesCounter);
+            //}
 
             GameObject UI = GameObject.FindGameObjectWithTag("UI");
             if (UI != null)
